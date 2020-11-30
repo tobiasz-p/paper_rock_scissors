8da2d19e432644499b8584d70d8d40102a922e56
tobiasz-p <Tobiasz@DESKTOP-CK0MGLO>
2020-11-30 20:59:44 +0100

    done

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import cv2
 import imutils
 import numpy as np
 from sklearn.metrics import pairwise
+import time
 
-bg = None
+background = None
 
-def run_avg(image, accumWeight):
-    global bg
-    # initialize the background
-    if bg is None:
-        bg = image.copy().astype("float")
+def avg(image, w):
+    global background
+    if background is None:
+        background = image.copy().astype("float")
         return
+    cv2.accumulateWeighted(image, background, w)
 
-    cv2.accumulateWeighted(image, bg, accumWeight)
 
-
-def segment(image, threshold=25):
-    global bg
-    diff = cv2.absdiff(bg.astype("uint8"), image)
-    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+def segmentation(image, th=25):
+    global background
+    diff = cv2.absdiff(background.astype("uint8"), image)
+    thresholded = cv2.threshold(diff, th, 255, cv2.THRESH_BINARY)[1]
     countours, _ = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if len(countours) == 0:
         return
@@ -26,84 +25,68 @@ def segment(image, threshold=25):
         segmented = max(countours, key=cv2.contourArea)
         return (thresholded, segmented)
 
-
-def count(thresholded, segmented, clone):
-    chull = cv2.convexHull(segmented)
-
-    extreme_top    = tuple(chull[chull[:, :, 1].argmin()][0])
-    extreme_bottom = tuple(chull[chull[:, :, 1].argmax()][0])
-    extreme_left   = tuple(chull[chull[:, :, 0].argmin()][0])
-    extreme_right  = tuple(chull[chull[:, :, 0].argmax()][0])
-
-    cX = int((extreme_left[0] + extreme_right[0]) / 2)
-    cY = int((extreme_top[1] + extreme_bottom[1]) / 2)
-
-    distance = pairwise.euclidean_distances([(cX, cY)], Y=[extreme_left, extreme_right, extreme_top, extreme_bottom])[0]
-    maximum_distance = distance[distance.argmax()]
-
-    radius = int(0.85 * maximum_distance)
-    l = (2 * np.pi * radius)
-
-    circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
-	
-    cv2.circle(circular_roi, (cX, cY), radius, 255, 1)
-    cv2.circle(clone, (cX, cY), radius, (0,255,0), 1)
-
-    circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
-    cv2.imshow("Mask", circular_roi)
-
-    (countours, _) = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def count(thresholded, segmented, frame_cp):
+    hull = cv2.convexHull(segmented)
+    top    = tuple(hull[hull[:, :, 1].argmin()][0])
+    bottom = tuple(hull[hull[:, :, 1].argmax()][0])
+    left   = tuple(hull[hull[:, :, 0].argmin()][0])
+    right  = tuple(hull[hull[:, :, 0].argmax()][0])
+    x = int((left[0] + right[0]) / 2)
+    y = int((top[1] + bottom[1]) / 2)
+    dist = pairwise.euclidean_distances([(x, y)], Y=[left, right, top, bottom])[0]
+    max_dist = dist[dist.argmax()]
+    r = int(0.8 * max_dist)
+    l = (2 * np.pi * r)
+    roi = np.zeros(thresholded.shape[:2], dtype="uint8")
+    cv2.circle(roi, (x, y), r, 255, 1)
+    cv2.circle(frame_cp, (x, y), r, (0,255,0), 1)
+    roi = cv2.bitwise_and(thresholded, thresholded, mask=roi)
+    (countours, _) = cv2.findContours(roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     count = 0
     for countour in countours:
-        (_, y, _, h) = cv2.boundingRect(countour)
-        if (1.25 * cY  > (y + h)) and (countour.shape[0] < (0.25 * l)):
+        _, y, _, h = cv2.boundingRect(countour)
+        if ((y + (y * 0.25)) > (y + h)) and ((l * 0.25) > countour.shape[0]):
             count += 1
-
     return count
 
 if __name__ == "__main__":
-    accumWeight = 0.5
-    path = "./videos/video-4.mkv"
-    camera = cv2.VideoCapture(path)
-    num_frames = 0
-    calibrated = False
+    w = 0.5
+    path = "./videos/2.mkv"
+    input = cv2.VideoCapture(path)
+    frames_counter = 0
+    result = None
 
     while(True):
-        (_, frame) = camera.read()
-
-        frame = imutils.resize(frame, width=700)
+        _, frame = input.read()
         frame = cv2.flip(frame, 1)
-        clone = frame.copy()
-
+        frame_cp = frame.copy()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        if num_frames < 30:
-            run_avg(gray, accumWeight)
+        if frames_counter < 30:
+            avg(gray, w)
         else:
-            hand = segment(gray)
+            hand = segmentation(gray)
             if hand is not None:
-                #result =''
                 (thresholded, segmented) = hand
-                cv2.drawContours(clone, [segmented], -1, (0, 0, 255))
-                fingers = count(thresholded, segmented, clone)
-                if fingers ==0:
+                cv2.drawContours(frame_cp, [segmented], -1, (0, 0, 255))
+                fingers = count(thresholded, segmented, frame_cp)
+                if fingers == 0:
                     result = "rock"
                 elif fingers == 2:
                     result = "scissors"
-                elif fingers ==5:
+                elif fingers == 5:
                      result = "paper"
-                     
-                cv2.putText(clone, result, (70, 45), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,0,255), 2)
+                if result is not None:
+                    cv2.putText(frame_cp, result, (70, 45), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,0,255), 2)
                 cv2.imshow("Thesholded", thresholded)
+                time.sleep(0.01)
 
-        num_frames += 1
-        cv2.imshow("Video", clone)
-
-        # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
-        if keypress == ord("q"):
+        frames_counter += 1
+        cv2.imshow("Video", frame_cp)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
             break
 
-camera.release()
+input.release()
 cv2.destroyAllWindows()
